08a00bebabedbeb855e5468351e467ea90a06860
avneesh0612 <avneeshagarwal0612@gmail.com>
2021-12-08 17:54:27 +0530

    completed tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,64 @@
-import Head from 'next/head'
+import { useState, useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
+import Image from "next/image";
 
 export default function Home() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  console.log(session);
+
+  async function signInWithTwitter() {
+    await supabase.auth.signIn({
+      provider: "twitter",
+    });
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+    <div className="flex flex-col items-center justify-center w-screen h-screen bg-[#D33943]">
+      {session ? (
+        <>
+          <Image
+            width={100}
+            height={100}
+            src={session.user.user_metadata.picture}
+            className="rounded-full"
+          />
+          <h3 className="mt-3 text-lg text-white">
+            Welcome {session?.user.user_metadata.full_name}
+          </h3>
+          <h3 className="mt-3 text-lg text-white">
+            You are signed in as {session?.user.email}
+          </h3>
+          <button
+            className="px-4 py-2 mt-3 text-black bg-white rounded-lg"
+            onClick={() => supabase.auth.signOut()}
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+            Sign out
+          </button>
+        </>
+      ) : (
+        <>
+          <h1 className="text-3xl font-semibold text-white">
+            How to add Twitter auth quickly with Supabase to your Next.js site
+            ⚡
+          </h1>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          <button
+            onClick={signInWithTwitter}
+            className="relative px-4 py-2 bg-[#1eb872] rounded-lg text-white mt-4"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+            Twitter sign in
+          </button>
+        </>
+      )}
     </div>
-  )
+  );
 }
